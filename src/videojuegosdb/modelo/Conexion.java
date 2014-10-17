21f98fb23f460e145256c8ab68ebef9f509f52a9
Victor <victor.z@ciencias.unam.mx>
2014-10-17 17:26:10 -0500

    Agregado método para obtener la conexión.

--- src/videojuegosdb/modelo/Conexion.java
+++ src/videojuegosdb/modelo/Conexion.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* Clase para la conexión a la base de datos. */
 package videojuegosdb.modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
+ * Clase para la conexión a la base de datos.
  *
- * @author victor
+ * @author Víctor Zamora Gutiérrez
+ * @version 1.0
  */
 public class Conexion {
-    
+
+    public static Connection c = null;
+
+    /** 
+     * Regresa la conexión a la base de datos.
+     * @return La conexión a la base de datos.
+     */
+    public static Connection getConexion() {
+        try {
+            if (c == null) {
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:videojuegos.db");
+            }
+            return c;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        return null;
+    }
+
 }
