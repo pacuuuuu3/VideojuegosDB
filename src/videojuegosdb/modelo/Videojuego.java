0338b2470ec9619b075708b2f0d8d6b5133bdb6b
Victor <victor.z@ciencias.unam.mx>
2014-10-16 23:04:14 -0500

    Probando git

---
+++ src/videojuegosdb/modelo/Videojuego.java
@@ -0,0 +1,18 @@
+/* Clase para abstraer un videojuego en la base de datos. */
+package videojuegosdb.modelo;
+
+/**
+ * Abstrae la información de un videojuego.
+ * @author Víctor Zamora Gutiérrez
+ * @version 1.0
+ */
+public class Videojuego {
+    private String nombre;
+    
+    public Videojuego(String nombre){
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
