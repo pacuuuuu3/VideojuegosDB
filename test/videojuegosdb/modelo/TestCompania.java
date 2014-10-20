8f5444c2186b6eb7063db92b2bfcecb07978e436
Victor <victor.z@ciencias.unam.mx>
2014-10-20 00:58:03 -0500

    Creadas clases de pruebas.

---
+++ test/videojuegosdb/modelo/TestCompania.java
@@ -0,0 +1,54 @@
+/* Clase para probar la clase Compañía. */
+package videojuegosdb.modelo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import videojuegosdb.modelo.*;
+
+/**
+ * Clase para probar la clase {@link Compania}
+ *
+ * @author Víctor Zamora Gutiérrez
+ * @version 1.0
+ */
+public class TestCompania {
+
+    /**
+     * Prueba unitaria para el método getId.
+     */
+    @Test
+    public void testGetId() {
+
+    }
+
+    /**
+     * Prueba unitaria para el método getCompania.
+     */
+    @Test
+    public void testGetCompania() {
+
+    }
+
+    /**
+     * Prueba unitaria para el método remove.
+     */
+    @Test
+    public void testRemove() {
+    }
+
+    /**
+     * Prueba unitaria para el método getAll.
+     */
+    @Test
+    public void testGetAll() {
+
+    }
+    
+    /**
+     * Prueba unitaria para el método getCompanias.
+     */
+    @Test
+    public void testGetCompanias(){
+    
+    }
+}
